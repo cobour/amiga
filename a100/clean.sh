514382edb1288020a1d395729c9032678a5db2a8
Frank <cobour@web.de>
2024-08-02 19:57:59 +0200

    added clean task

---
+++ a100/clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf dist/A100.adf
+rm -rf build/*.o
+rm -rf build/*.bb
+rm -rf data/converted/*.o
+rm -rf data/converted/*.dat
+rm -rf data/converted/*.dat.tmp
+rm -rf uae/dh0/A100
+rm -rf uae/dh0/*.dat
+rm -rf asm_files_index.i
+rm -rf files_index.i
